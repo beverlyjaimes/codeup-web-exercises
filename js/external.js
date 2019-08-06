c4bc9f99805d49f364415df473aa9a3b7086c344
beverlyjaimes <beverly.jaimespuente@gmail.com>
2019-08-06 09:46:55 -0500

    modified exercise

--- js/external.js
+++ js/external.js
@@ -7,38 +7,42 @@
 // // part 2 ------
 //
 // var userFavoriteColor = prompt("What is your favorite color?");
-// alert(userFavoriteColor + " is my favorite color too!");
-//
-//
-// // part 3 ------
-//
+// alert("Great " + userFavoriteColor + " is my favorite color too!");
+
+
+// part 3 ------
+
+// you can also convert to number by adding + instead of Number. If you want dollar sign add $ before end of string--------
+
 // var littleMermaidDaysRented = prompt("For how many days did you rent the Little Mermaid?");
 // var brotherBearDaysRented = prompt("For how many days did you rent the Brother Bear?");
 // var herculesDaysRented = prompt("For how many days did you rent the Hercules?");
 // var pricePerRentDayDollars = 3;
 // var totalDollarsSpentRenting = ((Number(littleMermaidDaysRented) + Number(brotherBearDaysRented) + Number(herculesDaysRented)) * pricePerRentDayDollars) ;
 //
-// alert("You will have to pay a total of " + totalDollarsSpentRenting + " dollars." ) ;
+// alert("You will have to pay a total of $" + totalDollarsSpentRenting.toFixed(2) + " dollars." ) ;
 
+//part 3--------------
+// //
 // var googleHourlyPayDollars = prompt("How much does Google pay you per hour?");
-// // var googleHoursWorkedWeek = prompt("How many hours did you work with Google this week?");
-// //
-// // var amazonHourlyPayDollars = prompt("How much does Amazon pay you per hour?");
-// // var amazonHoursWorkedWeek = prompt("How many hours did you work with Amazon this week?");
-// //
-// // var facebookHourlyPayDollars = prompt("How much does Facebook pay you per hour?");
-// // var facebookHoursWorkedWeek = prompt("How many hours did you work with Facebook this week?");
-// //
-// // var totalGooglePaymentDollars = (Number(googleHourlyPayDollars) * (Number(googleHoursWorkedWeek)));
-// // var totalAmazonPaymentDollars = (Number(amazonHourlyPayDollars) * (Number(amazonHoursWorkedWeek)));
-// // var totalFacebookPaymentDollars = (Number(facebookHourlyPayDollars) * (Number(facebookHoursWorkedWeek)));
-// //
-// // var totalWeeklyPaymentDollars = (Number(totalGooglePaymentDollars) + (Number(totalAmazonPaymentDollars) + (Number(totalFacebookPaymentDollars))));
-// //
-// // alert("You will make a total of " + totalWeeklyPaymentDollars + " dollars this week.");
-
-
+// var googleHoursWorkedWeek = prompt("How many hours did you work with Google this week?");
 //
+// var amazonHourlyPayDollars = prompt("How much does Amazon pay you per hour?");
+// var amazonHoursWorkedWeek = prompt("How many hours did you work with Amazon this week?");
+//
+// var facebookHourlyPayDollars = prompt("How much does Facebook pay you per hour?");
+// var facebookHoursWorkedWeek = prompt("How many hours did you work with Facebook this week?");
+//
+// var totalGooglePaymentDollars = (Number(googleHourlyPayDollars) * (Number(googleHoursWorkedWeek)));
+// var totalAmazonPaymentDollars = (Number(amazonHourlyPayDollars) * (Number(amazonHoursWorkedWeek)));
+// var totalFacebookPaymentDollars = (Number(facebookHourlyPayDollars) * (Number(facebookHoursWorkedWeek)));
+//
+// var totalWeeklyPaymentDollars = (Number(totalGooglePaymentDollars) + (Number(totalAmazonPaymentDollars) + (Number(totalFacebookPaymentDollars))));
+//
+// alert("You will make a total of $" + totalWeeklyPaymentDollars.toFixed(2) + " dollars this week.");
+// //
+// //
+// // ------------------------------------------------------------------------------------------------------------------
 // alert("Lets check if we can register you for Codeup");
 // var classFull = window.confirm ("Is this class full?\n (cancel=no ok=yes)");
 // if (classFull) {
@@ -51,10 +55,10 @@
 //     } else {
 //         alert("Sorry, better luck next time!")
 //     }
+
+}
 //
-// }
-
-
+//
 // var itemsPurchased = prompt("How many items have you purchased?");
 // var statusPremium =window.confirm("Are you a Premium member?\n (cancel=no ok=yes)");
 // var offerValidDate = prompt("What is the offer expiration year?");
@@ -64,10 +68,10 @@
 // } else {
 //     alert("Try again next time!")
 // }
-
-
-
-
-
-
-
+//
+//
+//
+//
+//
+//
+//
