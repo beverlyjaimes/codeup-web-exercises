f1ce9d4f366cea03cf8205e505678bf8df8c83e8
beverlyjaimes <beverly.jaimespuente@gmail.com>
2019-08-07 16:03:18 -0500

    condiditional notes

---
+++ js/conditionals-lec.js
@@ -0,0 +1,64 @@
+
+// var pizzaPreference = prompt("What kind of pizza do you like?");
+//
+// if (pizzaPreference === "pineapple and hot sauce") {
+//     alert("What a coincidence, that's my favorite!");
+// } else if (pizzaPreference === "cheese") {
+//     alert("Just plain cheese? Okay...");
+// } else {
+//     alert(pizzaPreference + " isn't my favorite, but let's order some!");
+// }
+
+// function evaluateIntersection(lightColor) {
+//     if(lightColor === "red"){
+//         alert("STOP!");
+//     } else if (lightColor === "yellow"){
+//         alert("Slow down!");
+//     } else if (lightColor === "green"){
+//         alert("go!");
+//     } else {
+//         alert("something!");
+//     }
+//
+// }
+// console.log(evaluateIntersection("red"));
+
+//
+// shopping not complete
+// child will begin at $5
+//
+// if shopping is complete
+// ichild earns an additional 5$
+//
+//
+// //------------------------------------------
+//
+//
+// if they choose sunny
+//
+// else if
+// a message will display:
+//     It is nice and sunny outside today. Wear shorts! Go to the beach, or the park, and get an ice cream.
+//     when they choose rainy
+//
+//
+//     a message will display:
+//     Rain is falling outside; take a rain coat and a brolly, and don't stay out for too long.
+//     when they choose snowing
+// a message will display:
+//     The snow is coming down — it is freezing! Best to stay in with a cup of hot chocolate, or go build a snowman.
+//
+//     when they choose overcast
+// a message will display:
+//     It isn't raining, but the sky is grey and gloomy; it could turn any minute, so take a rain coat just in case.
+//
+// else
+// user select the weather
+// nothing is chosen nothing will display
+//
+//
+// //--------------------------------------------
+//
+//
+// if black is selected background is black text is white
+// else nothing chosen default  white is selected background is white text is black
