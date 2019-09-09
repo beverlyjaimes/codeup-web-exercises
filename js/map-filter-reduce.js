6e5f18cb8feb673480212132d51151234a7f98c2
beverlyjaimes <beverly.jaimespuente@gmail.com>
2019-09-09 15:11:43 -0500

    exercise after walkthrough

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -44,31 +44,24 @@ const users = [
 //     Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
 
 //
-let threeLang = users.filter(person => person.languages.length >= 3);
+let threeLang = users.filter(user => user.languages.length >= 3);
 
 console.log(threeLang);
 
 
 //     Use .map to create an array of strings where each element is a user's email address
 
-let emails = users.map(person =>  person.email);
+let emails = users.map(user =>  user.email);
 
 console.log(emails);
 // Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
 //
 let exp = users.map(person => person.yearsOfExperience);
 console.log(exp);
-//
-// let total = exp.reduce((a, b) => a + b, 0);
-//
-// console.log(total);
-//
-// let average = total / users.length;
-//
-// console.log(average);
 
-const total = exp.reduce((accumulation , currentNumber) => {
-    return accumulation + currentNumber;
+
+const total = exp.reduce((total , user) => {
+    return total + user;
 } , 0);
 
 console.log(total);
@@ -81,7 +74,7 @@ console.log(average);
 
 //     Use .reduce to get the longest email from the list of users.
 
-let longest = emails.reduce((a, b) => { return a.length > b.length ? a : b; });
+let longest = emails.reduce((longestEmail, user) => { return longestEmail.length > user.length ? longestEmail : user; });
 
 console.log(longest);
 
@@ -92,8 +85,14 @@ console.log(longest);
 //     Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
 
 
-let usersNames = users.reduce((accumulation, user) => {
-    return accumulation + '' + user.name + ' '
+let usersNames = users.reduce((accumulation, user, i) => {
+    if (i < users.length -1) {
+    return accumulation +=  user.name + ', ';
+    } else {
+        accumulation +=  `and ${user.name}.`
+    }
+    return accumulation
+
 }, "Your instructors are :");
 
 
