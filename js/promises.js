4a3c23575d5c8cee791c601b6a22d5dede96b960
beverlyjaimes <beverly.jaimespuente@gmail.com>
2019-09-10 13:57:32 -0500

    last commit

---
+++ js/promises.js
@@ -0,0 +1,54 @@
+//
+// const wait = (number) => {
+//     return new Promise((resolve) => {
+//         setTimeout( () => {
+//          resolve();
+//         }, number);
+//
+//     });
+//
+// };
+
+//Simplified!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!-------------
+
+// const wait = (number) => {
+//     return new Promise((resolve) => {
+//         setTimeout(resolve, number);
+//
+//     });
+//
+// };
+//
+//
+// // console.log());
+//
+// //
+// wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+// wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
+//API request using FETCH -----------------------------------------------------------------------
+
+// fetch("https://swapi.co/api/people/1")
+//     .then((response) => {
+//     return response.json();
+// }).then((data) => {
+//     console.log(data);
+// }).catch(console.log);
+//
+
+
+
+function commit (userName) {
+    fetch(`https://api.github.com/users/${userName}/events/public`, {headers: {"Authorization": gitToken}})
+
+        .then((response) => {
+            console.log(response);
+            return response.json();
+        }).then((data) => {
+        console.log(data);
+    }).then((dat) =>
+);
+
+}
+
+console.log(commit("beverlyjaimes"));
